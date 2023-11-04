e87c6c121dfeb112143374c2da78c6745b922c70
İsmail Özer <ismailozer35041@gmail.com>
2023-11-04 10:34:29 +0300

    Add project files.

---
+++ NLayer.Service/Exceptions/ClientSideExpection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Exceptions
+{
+    public class ClientSideExpection:Exception
+    {
+        public ClientSideExpection(string message):base(message) 
+        {
+            
+        }
+    }
+}
